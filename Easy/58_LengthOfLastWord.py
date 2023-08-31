bdb4421b2d152dda2812facebb4a3615e807cee6
Tejesh <teju708@gmail.com>
2023-08-31 00:44:40 -0700

    modify solution

--- Easy/58_LengthOfLastWord.py
+++ Easy/58_LengthOfLastWord.py
@@ -1,10 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()
-        length = 0
-        for i in range(len(s)-1, -1, -1):
-            if(s[i]!= " "):
-                length +=1
-            else: 
-                break
-        return length
+        a = s.split(" ")
+        return (len(a[-1]))
