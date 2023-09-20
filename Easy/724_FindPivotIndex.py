b31ff62c98d33d5f3799116d71afb77e10a8ff46
Tejesh <tpaidi@asu.edu>
2023-09-19 18:42:19 -0700

    update solns

--- Easy/724_FindPivotIndex.py
+++ Easy/724_FindPivotIndex.py
@@ -1,16 +1,10 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        pre = []
-        post = []
-        pre.append(0)
-        pre.append(nums[0])
-        for i in range(1, len(nums)):
-            pre.append(pre[-1]+ nums[i])
-        post.append(nums[-1])        
-        post.append(0)
-        for i in range( len(nums)-2, -1, -1):
-            post.insert(0, post[0]+ nums[i])
-        for i in range(1,len(pre)):
-            if(pre[i-1]==post[i]):
-                return i-1
+        tot = sum(nums)
+        leftS = 0
+        for i in range(len(nums)):
+            rightS = tot - leftS - nums[i]
+            if(leftS == rightS):
+                return i
+            leftS += nums[i]
         return -1
